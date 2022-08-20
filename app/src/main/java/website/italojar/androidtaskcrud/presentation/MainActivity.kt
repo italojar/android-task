f897ffc5af47e6fa6356deec62e9c788e8ef4fb4
italojar <jaramilloarmijositalo@gmail.com>
2022-08-20 13:17:48 +0200

    Create packages project structure

--- app/src/main/java/website/italojar/androidtaskcrud/presentation/MainActivity.kt
+++ app/src/main/java/website/italojar/androidtaskcrud/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package website.italojar.androidtaskcrud
+package website.italojar.androidtaskcrud.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import website.italojar.androidtaskcrud.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
